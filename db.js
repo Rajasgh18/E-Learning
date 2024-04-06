9a922c2dde45f067670d0016283147a0f6610425
Raja Singh <rajasgh18@gmail.com>
2024-04-06 23:09:14 +0530

    updated postgress connection

--- db.js
+++ db.js
@@ -1,23 +1,39 @@
-const postgres = require('postgres');
+const { Pool } = require('pg');
 
-let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
-
-const sql = postgres({
-    host: PGHOST,
-    database: PGDATABASE,
-    username: PGUSER,
-    password: PGPASSWORD,
-    port: 5432,
-    ssl: 'require',
-    connection: {
-        options: `project=${ENDPOINT_ID}`,
+// Create a pool with environment variables
+const pool = new Pool({
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    ssl: {
+        rejectUnauthorized: false,
     },
+    sslmode: 'require',
 });
 
-async function getPgVersion() {
-    const result = await sql`select version()`;
-    if(result)
-        console.log("PostgresSQL is Connected!")
-}
+// Connecting to PostgreSQL
+pool.connect(async (err, client, done) => {
+    if (err)
+        return console.error('Error connecting to PostgreSQL:', err);
+    console.log('Connected to PostgreSQL!');
 
-module.exports = getPgVersion;
+    const userTableExists = await client.query(`SELECT EXISTS (
+        SELECT 1
+        FROM pg_tables
+        WHERE tablename = 'users'
+    )`);
+    if (!userTableExists.rows[0].exists) {
+        await client.query(`CREATE TABLE users(
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(20),
+            email VARCHAR(30),
+            password VARCHAR(100),
+            profile_pic VARCHAR(100)
+            )`);
+        console.log("Created User table!");
+    }
+
+});
+
+module.exports = pool;
