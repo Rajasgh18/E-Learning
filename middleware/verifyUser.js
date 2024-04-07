5b0cb77ce01992cfb57829a11ea181e7baf88520
Raja Singh <rajasgh18@gmail.com>
2024-04-07 23:30:35 +0530

    JWT middleware

---
+++ middleware/verifyUser.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const { SECRET_KEY } = process.env
+
+const verifyToken = (req, res, next) => {
+    const token = req.headers.authorisation || req.query.token;
+
+    if (!token) return res.status(403).send("A token is required for authentication");
+
+    try {
+        const decoded = jwt.verify(token, SECRET_KEY);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        return res.status(401).send('Invalid token');
+    }
+}
+
+module.exports = verifyToken;
